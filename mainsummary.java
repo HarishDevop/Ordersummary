0b016c426a7e2681e72357e0d3cd6a06af336459
Gopi <Gopi@gmail.com>
2020-11-10 11:33:31 +0000

    Added changes for the new branch feature/twitter

--- mainsummary.java
+++ mainsummary.java
@@ -2,7 +2,7 @@ public class mainsummary {
 	public static void main(string[] args) {
 		//Prints "Hello, world" in the terminal window
 	system.out.println("Hello,World");
-	//Print "Welcome to git"
+	//Prints,"Created new branch for restricting the changes
 	system.out.println("welcome to git");
 	}
 }
