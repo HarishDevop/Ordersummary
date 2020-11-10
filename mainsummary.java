8074f93bc59a7f963ae7a732622baa1e2049910d
Gopi <Gopi@gmail.com>
2020-11-10 09:37:22 +0000

    Added a comment line to mainsummary.java

--- mainsummary.java
+++ mainsummary.java
@@ -2,6 +2,7 @@ public class mainsummary {
 	public static void main(string[] args) {
 		//Prints "Hello, world" in the terminal window
 	system.out.println("Hello,World");
+	//Print "Welcome to git"
 	system.out.println("welcome to git");
 	}
 }
